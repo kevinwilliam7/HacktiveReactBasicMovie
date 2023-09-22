7ebb40fde768a3953e760d282bab5ea993189539
Untan <untan@Untans-Mac-mini.local>
2023-09-22 11:22:20 +0700

    change url

--- src/components/movie/slider/index.js
+++ src/components/movie/slider/index.js
@@ -19,7 +19,7 @@ export default function Slider(props) {
 				movies.length !== 0 ? movies.map((val) => {
 					return <SwiperSlide key={val.id} className='p-2'>
 						<div className='group relative max-w-full rounded-lg overflow-hidden'>
-							<Link href={{pathname: '/detail', query: `id=${val.id}`}}>
+							<Link href={`/detail?id=${val.id}`}>
 								<div className="bg-red-100 w-44 h-64 relative group max-w-full rounded-lg">
 									<Image className='group-hover:scale-125 group-hover:blur-sm rounded-lg h-64 w-44' src={`https://image.tmdb.org/t/p/original/${val.poster_path}`} width={250} height={250} alt='poster' />
 									<div className="group-hover:h-full group-hover:opacity-100 absolute top-0 w-full h-0 flex flex-col justify-center items-center hover:bg-transparent opacity-0 duration-500">
@@ -28,7 +28,7 @@ export default function Slider(props) {
 								</div>
 							</Link>
 						</div>
-						<Link href={{pathname: '/detail', query: `id=${val.id}`}}>
+						<Link href={`/detail?id=${val.id}`}>
 							<div className='p-2 group group-hover:scale-125'>
 								<h1 className="truncate overflow-hidden text-sm text-gray-900 font-semibold justify-center">{val.title} {' ('+new Date(val.release_date).getFullYear()+')'}</h1>
 								<p className='truncate text-xs text-gray-900 font-normal'>{Date(val.release_date)}</p>
