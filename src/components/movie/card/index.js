d010306140efa307a20e1e123df6e660109a2ba8
Kevin William <kevonwojdkzjd@gmail.com>
2023-09-23 14:01:49 +0700

    fix image card component

--- src/components/movie/card/index.js
+++ src/components/movie/card/index.js
@@ -10,8 +10,15 @@ export default function Card(props) {
     return (
         <div className='group relative max-w-full rounded-lg overflow-hidden'>
             <Link href={{ pathname: `/detail`, query:`id=${val.id}` }} >
-                <div className={`${isError === val.id ? `animate-pulse bg-gray-300` : `bg-red-100`} ${size === `large` ? `w-full` : `w-44 h-64`} relative group max-w-full rounded-lg`}>
-                    <Image onError={()=>setIsError(val.id)} className={`${size === `large` ? `object-fit h-44 md:h-64 lg:h-72 xl:h-80 w-full` : `w-44 h-64`} group-hover:scale-125 group-hover:blur-sm rounded-lg`} src={`https://image.tmdb.org/t/p/original/${size === `large` ? val.backdrop_path : val.poster_path}`} width={250} height={250} alt='poster' />
+                <div className={`${(isError === val.id) ? `animate-pulse bg-gray-300` : `bg-red-100`} ${size === `large` ? `w-full` : `w-44 h-64`} relative group max-w-full rounded-lg`}>
+                    <Image 
+                        onError={()=>setIsError(val.id)}
+                        className={`${size === `large` ? `object-fit h-44 md:h-64 lg:h-72 xl:h-80 w-full` : `w-44 h-64`} group-hover:scale-125 group-hover:blur-sm rounded-lg`} 
+                        src={`https://image.tmdb.org/t/p/original/${size === `large` ? val.backdrop_path : val.poster_path}`} 
+                        width={250} 
+                        height={250} 
+                        alt={(isError === val.id) ? `` : `poster`} 
+                    />
                     <div className="group-hover:h-full group-hover:opacity-100 absolute top-0 w-full h-0 flex flex-col justify-center items-center hover:bg-transparent opacity-0 duration-500">
                         <FaPlay className='text-orange-400' size={'30px'}/>
                     </div>
